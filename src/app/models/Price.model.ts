98d75088bc2038144c1b23de3a2d7c3b47513338
Bodo8 <b.trojanski@gmail.com>
2025-10-07 12:24:20 +0200

    Usunięcie zbednych

--- src/app/models/Price.model.ts
+++
@@ -1,8 +0,0 @@
-export class Price {
-  constructor(public baseValue: number) {}
-
-  applyDiscount(discountPercent: number): number {
-    const discount = (this.baseValue * discountPercent) / 100;
-    return this.baseValue - discount;
-  }
-}
--- src/app/models/Rabat.model.ts
+++
@@ -1,3 +0,0 @@
-export class Rabat {
-  constructor(public percent: number) {}
-}
