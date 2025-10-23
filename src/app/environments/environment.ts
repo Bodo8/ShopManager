de00871921a7386e98da6f739ce5128fda9a6191
Bodo8 <b.trojanski@gmail.com>
2025-10-23 09:24:59 +0200

    Poprawione tło

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
- // baseUrl: 'http://localhost:5138/api'
-  baseUrl: 'http://192.109.241.252:80/api'
+  baseUrl: 'http://localhost:5138/api'
+ // baseUrl: 'http://192.109.241.252:80/api'
 };
